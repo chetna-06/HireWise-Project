eeaa77f7d0d6c22a7805a4202356a36d45127f08
chetna-06 <chetna20061984@gmail.com>
2026-09-20 18:32:23 +0530

    fix clerk webhook verification

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -4,12 +4,13 @@ import User from "../models/User.js";
 export const clerkWebhooks=async(req,res)=>{
     try{
         const whook=new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-        await whook.verify(JSON.stringify(req.body),{
+        await whook.verify(req.body,{
             "svix-id":req.headers["svix-id"],
             "svix-timestamp":req.headers["svix-timestamp"],
             "svix-signature":req.headers["svix-signature"]
         })
-        const { data,type}=req.body
+        // const { data,type}=req.body
+        const { data,type}=JSON.parse(req.body.toString())
         switch(type){
             case 'user.created':{
                 const userData={
